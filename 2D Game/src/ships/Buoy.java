5baf47080b8e6592268989126812393dbe5c2d86
doominabox1 <jrddupont@gmail.com>
2017-02-10 10:55:46 -0600

    Spatial Hash testing

---
+++ 2D Game/src/ships/Buoy.java
@@ -0,0 +1,45 @@
+package ships;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Vector2f;
+
+import util.Utility;
+import def.Renderable;
+
+public class Buoy  extends Ship implements Renderable{
+
+	public Buoy(int xSize, int ySize, String spriteSheetPath, int spriteSize) throws SlickException {
+		super(1, 1, spriteSheetPath, spriteSize);
+		ShipPart sp = new ShipPart(ShipPart.TIER1, ShipPart.NOTHING, ShipPart.NOTHING, ShipPart.NOTHING);
+		this.setPart(sp, 0, 0);
+		updateBlocks();
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics g, Rectangle camera) {
+		if(positionOnScreen == null){
+			positionOnScreen = new Vector2f(gc.getWidth() / 2, gc.getHeight() / 2);
+		}
+		if(!(camera.contains(getSimpleClippingHull()) || camera.intersects(getSimpleClippingHull()))){
+			return;
+		}
+		Vector2f drawPosition = new Vector2f(position);
+		drawPosition.sub(camera.getLocation());
+		for (int i = 0; i < ship.length; i++) {			// Draw each ship component
+			for (int j = 0; j < ship[0].length; j++) {
+				Image curImage = hullList[i][j];
+				if(curImage == null){
+					continue;
+				}
+				Vector2f p = Utility.getVectorFromArrayLocation(i, j, shipSize, ship);
+				curImage.setCenterOfRotation(center.x - p.x, center.y - p.y);
+				curImage.setRotation((float) angle);
+				curImage.draw(drawPosition.x - center.x + p.x, drawPosition.y - center.y + p.y, (float) shipSize, (float) shipSize);
+			}
+		}
+	}
+}
